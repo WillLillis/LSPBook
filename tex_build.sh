19ebed6218187adf2794d4aada6851e816871236
WillLillis <wlillis@umass.edu>
2024-01-31 23:45:45 -0500

    Some progress

--- tex_build.sh
+++ tex_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-tectonic -o build/ build_book.tex
+tectonic -o build/ build_book.tex 
 makeindex build/book.idx -s indexstyle.ist
 biber main/book.tex
-tectonic -o build/ build_book.tex
+tectonic -o build/ build_book.tex 
 xpdf build/build_book.pdf
